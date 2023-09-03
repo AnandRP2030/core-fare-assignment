ae3356af0dff3a6b78ffb5d5e32d3b703bda46de
AnandRP2030 <anand.rp2030@gmail.com>
2023-09-03 14:59:45 +0530

    initial app structure is created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Navbar} from './components/navbar/navbar';
+import { Footer } from './components/footer/footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
